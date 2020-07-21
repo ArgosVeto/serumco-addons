fd8a26ea02145a966d4c3e8bf237ae7b5b1b09c8
Omar OUHARI <omar.ouhari@gmail.com>
2020-07-21 16:04:03 +0200

    [FIX] : undo init

--- argos-addons/argos_base/models/product_template.py
+++ argos-addons/argos_base/models/product_template.py
@@ -6,9 +6,9 @@ from odoo import models, fields, registry, http, api, _
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    mother_class = fields.Char(string="Class")
-    child_class = fields.Char(string="Sub Class")
-    sub_child_class = fields.Char(string="Sub child class")
-    gtin = fields.Char(string="GTIN Code")
-    ean = fields.Char(string="EAN Code")
-    cip = fields.Integer(string="CIP Code")
+    mother_class = fields.Char('Class')
+    child_class = fields.Char('Sub Class')
+    sub_child_class = fields.Char('Sub child class')
+    gtin = fields.Char('GTIN Code')
+    ean = fields.Char('EAN Code')
+    cip = fields.Integer('CIP Code')
