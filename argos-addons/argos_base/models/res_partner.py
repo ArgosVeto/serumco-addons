979854ee7d1f0b277246c7d0f481e7e0e1c115c6
Omar OUHARI <omar.ouhari@gmail.com>
2020-08-10 17:19:41 +0200

    [FIX] : add needed access right to the mrdv user to create user correctly

--- argos-addons/argos_base/models/res_partner.py
+++ argos-addons/argos_base/models/res_partner.py
@@ -15,6 +15,9 @@ class ResPartner(models.Model):
     send_sms = fields.Boolean('Send Sms')
     to_call = fields.Boolean('Call')
     origin_id = fields.Many2one('connection.origin', 'Connection Origin')
+    signup_expiration = fields.Datetime(groups='base.group_erp_manager,argos_base.mrdv_group_user')
+    signup_token = fields.Char(groups='base.group_erp_manager,argos_base.mrdv_group_user')
+    signup_type = fields.Char(groups='base.group_erp_manager,argos_base.mrdv_group_user')
 
     @api.model
     def _get_partner_by_name(self, name=False, phone=False):
