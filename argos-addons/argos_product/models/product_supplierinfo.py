0c113eb537c58552fd97dd548030821d36328562
solo-etech <solojr.etech@gmail.com>
2020-09-11 17:53:46 +0300

    [IMPL] #ARGPAT-5 : import gtin mgtin

--- argos-addons/argos_product/models/product_supplierinfo.py
+++ argos-addons/argos_product/models/product_supplierinfo.py
@@ -7,3 +7,5 @@ class ProductSupplierinfo(models.Model):
     _inherit = 'product.supplierinfo'
 
     maker_partner_id = fields.Many2one('res.partner', 'Product Maker')
+    is_discount = fields.Boolean('Is discount', default=False)
+    is_import = fields.Boolean('Is import', default=False)
