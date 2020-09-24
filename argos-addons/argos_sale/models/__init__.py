a886632a0f9143e7d0edea41c2f8afd6255033d1
Vallois <avallois@student.42.fr>
2020-09-24 13:42:40 +0200

    [IMPL] #ARGP-119: add euthanasia feature + act definition

--- argos-addons/argos_sale/models/__init__.py
+++ argos-addons/argos_sale/models/__init__.py
@@ -6,3 +6,4 @@ from . import sale_coupon_program
 from . import res_partner
 from . import sale_order_line
 from . import product_template
+from . import sale_make_invoice_advance
--- argos-addons/argos_sale/models/res_partner.py
+++ argos-addons/argos_sale/models/res_partner.py
@@ -7,7 +7,19 @@ import ast
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    consultation_ids = fields.One2many('sale.order', 'patient_id', 'Consultations')
+    consultation_ids = fields.Many2many('sale.order', compute='_compute_consultation_ids', string='Consultations')
+    act_ids = fields.Many2many('sale.order.line', compute='_compute_act_ids', string='Acts')
+
+    def _compute_consultation_ids(self):
+        for rec in self:
+            consultations = self.env['sale.order'].search([('is_consultation', '=', True), ('patient_id', '=', rec.id)])
+            rec.consultation_ids = [(6, 0, consultations.ids)]
+
+    def _compute_act_ids(self):
+        for rec in self:
+            acts = self.env['sale.order.line'].search([('order_id.is_consultation', '=', True), ('product_id.act_type', '!=', 'undefined'),
+                                                          ('order_id.patient_id', '=', rec.id)])
+            rec.act_ids = [(6, 0, acts.ids)]
 
     def button_create_consultation(self):
         self.ensure_one()
