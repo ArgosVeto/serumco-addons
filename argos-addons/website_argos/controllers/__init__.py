1f5a90a50efa5845478813e9926c5ee0c62e924d
Bizople <code@bizople.com>
2020-09-29 14:23:05 +0530

    [REMOVE] website addons

--- argos-addons/website_argos/controllers/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Developed by Bizople Solutions Pvt. Ltd.
-# See LICENSE file for full copyright and licensing details
-
-from . import main
-from . import clinic_main
-from . import protal
-from . import address
-# from . import cart_address
