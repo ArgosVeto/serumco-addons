ab5caf29b35970a5b5e9c0007723551d11a1afb1
Nikhil <nikhil@gmail.com>
2020-10-30 17:04:13 +0530

    deleted duplicate modules and added seo modules, fix design issue in slider snippet.

--- extra-addons/website_seo/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#################################################################################
-#
-#    Copyright (c) 2018-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>)
-#   See LICENSE URL <https://store.webkul.com/license.html/> for full copyright and licensing details.
-#################################################################################
-from . import res_config
-from . import website_seo
-from . import ir_qweb
