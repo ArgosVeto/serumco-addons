e6f254caf4766695fc3cf54f098dd577987f3d26
devteamstephane87 <devteamstephane87>
2020-11-05 12:23:05 +0530

    [ADD][NEW] category in the footer and add category in the footer

--- serum-co-addons/website_argos/controllers/clinic_main.py
+++ serum-co-addons/website_argos/controllers/clinic_main.py
@@ -526,7 +526,7 @@ class ClinicDetail(http.Controller):
 								not_working_time = True
 					time_range = {'start_time':start_time,'end_time':end_time}
 					working_timing.update({week_slot[str(day_week)]:time_range})	
-		values = {'service_ids':service_ids,'operating_unit':operating_unit_id,'working_timing':working_timing,'not_working_time':not_working_time}
+		values = {'service_ids':service_ids, 'payment_method_ids':payment_method_ids, 'operating_unit':operating_unit_id,'working_timing':working_timing,'not_working_time':not_working_time}
 		return request.env['ir.ui.view'].render_template("website_argos.clinic_pratice",values)
 
 class ClinicContact(http.Controller):	
