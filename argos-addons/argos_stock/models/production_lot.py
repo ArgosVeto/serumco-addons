1ba0e1bebb9782906a9f8258e4dd1a72e3879738
m0r7y <r.radoniaina11@gmail.com>
2020-12-07 14:13:09 +0300

    [FIX] #ARGP-83: translation

--- argos-addons/argos_stock/models/production_lot.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-from odoo import models, fields
-
-
-class StockProductionLot(models.Model):
-    _inherit = 'stock.production.lot'
-
-    life_date = fields.Datetime(string='End of Life Date')
