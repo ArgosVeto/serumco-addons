e2f3a58ed3602444f5699143ba13ecd9ba648c7b
Loubna BEN MOUSSA <loubna.benmoussa@arkeup.com>
2020-12-07 22:44:57 +0100

    [IMPL] #ARGP-48: Init Module INVENTORY

---
+++ argos-addons/argos_stock/models/stock_inventory.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'stock.inventory'
+
+    name = fields.Char(default=fields.Date.today().strftime("%d/%m/%Y"))
+    prefill_counted_quantity = fields.Selection(default='zero', readonly=True)
