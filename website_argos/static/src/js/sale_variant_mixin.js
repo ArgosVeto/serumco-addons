3d747b70d6c4f1fb5d2bc7708ae109b93fb5874a
samuel <s.ramarosely@etechconsulting-mg.com>
2022-01-11 16:06:03 +0300

    [FIW] ARGPWEB-7 display radio button

--- website_argos/static/src/js/sale_variant_mixin.js
+++ website_argos/static/src/js/sale_variant_mixin.js
@@ -4,7 +4,7 @@ odoo.define('websiteArgos.VariantMixin', function (require) {
     var publicWidget = require('web.public.widget');
     publicWidget.registry.WebsiteSale.include({
         _onChangeCombination: function (ev, $parent, combination) {
-            var $price_total = $parent.find("#add_to_cart .oe_currency_value");
+            var $price_total = $parent.find("#add_to_cart .product_price .oe_price .oe_currency_value");
             $price_total.text(this._priceToStr(combination.price_total));
             this._super.apply(this, arguments);
         }
