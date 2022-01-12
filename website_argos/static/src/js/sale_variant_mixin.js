3636921eeaf38db9de8535a457a75df9b3b133d3
samuel <s.ramarosely@etechconsulting-mg.com>
2022-01-12 14:23:10 +0300

    [FIX] ARGPWEB-7 use js instead of href to redirect alt_product

--- website_argos/static/src/js/sale_variant_mixin.js
+++ website_argos/static/src/js/sale_variant_mixin.js
@@ -3,10 +3,19 @@ odoo.define('websiteArgos.VariantMixin', function (require) {
 
     var publicWidget = require('web.public.widget');
     publicWidget.registry.WebsiteSale.include({
+        events: _.extend({}, publicWidget.registry.WebsiteSale.prototype.events,{
+            'click label.alt_product_radio[data-url]': '_onClickAltProductRadio',
+        }),
         _onChangeCombination: function (ev, $parent, combination) {
             var $price_total = $parent.find("#add_to_cart .product_price .oe_price .oe_currency_value");
             $price_total.text(this._priceToStr(combination.price_total));
             this._super.apply(this, arguments);
+        },
+        _onClickAltProductRadio: function (ev) {
+            ev.preventDefault();
+            ev.stopPropagation();
+            var $alt_product = $(ev.currentTarget);
+            window.location.replace($alt_product.data('url'));
         }
     });
 
